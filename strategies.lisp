51344715b70b3b55170ab09da18cdeac43390011
Nicolas Avrutin <nicolasavru@gmail.com>
2013-09-09 03:48:59 -0400

    implement alpha-beta search

--- strategies.lisp
+++ strategies.lisp
@@ -46,6 +46,40 @@
         (declare (ignore value))
         move)))
 
+(defun alpha-beta (player board achievable cutoff ply eval-fn)
+  "Find the best move for PLAYER according to EVAL-FN, searching PLY
+  levels deep. Don't call it with PLY 0."
+  (if (= ply 0)
+      (funcall eval-fn player board)
+      (let ((moves (legal-moves player board)))
+        (if (null moves)
+            (if (null (legal-moves (opponent player) board))
+                (- (alpha-beta (opponent player) board
+                            (- cutoff) (- achievable)
+                            (- ply 1) eval-fn))
+                0) ;; do something useful here?
+            (do* ((best-move (car moves))
+                  (moves2 moves (cdr moves))
+                  (move (car moves2) (car moves2))
+                  (board2 (make-move move (copy-board board)))
+                  (val (- (alpha-beta (opponent player) board2
+                                      (- cutoff) (- achievable)
+                                      (- ply 1) eval-fn))))
+                 ((or (null moves2) (>= achievable cutoff))
+                  (values achievable best-move))
+              (when (> val achievable)
+                (setf achievable val)
+                (setf best-move move)))))))
+
+(defun alpha-beta-searcher (ply eval-fn)
+  "A streatgy that searches PLY levels and then uses EVAL-FN."
+  #'(lambda (player board)
+      (multiple-value-bind (value move)
+          (alpha-beta player board
+                      most-positive-fixnum most-negative-fixnum
+                      ply eval-fn)
+        (declare (ignore value))
+        move)))
 
 (defun human (player board)
   "A human player for the game of Checkers"
